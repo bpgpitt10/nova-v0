38425d8f1aff3a0df3575b804c0d8e491019e162
bpgpitt10 <bpgpitt10@gmail.com>
2026-09-05 16:37:58 -0400

    Export intelligence inputs and Pattern Stability shadow model

--- src/intelligence/index.ts
+++ src/intelligence/index.ts
@@ -11,8 +11,24 @@ export type {
   IntelligenceTraceValue,
 } from './contracts'
 
+export type {
+  IntelligenceAnalysisDataset,
+  IntelligenceClubId,
+  IntelligenceSession,
+  IntelligenceShot,
+  PatternStabilityInput,
+  PatternStabilityInputShot,
+} from './input'
+
 export {
   getDownstreamConsumers,
   getIntelligenceModel,
   listIntelligenceModels,
 } from './registry'
+
+export {
+  patternStabilityLegacyV1Config,
+  type PatternStabilityConfig,
+} from './models/patternStability/config'
+export { patternStabilityDefinition } from './models/patternStability/definition'
+export { calculatePatternStabilityLegacyV1 } from './models/patternStability/legacyV1'
