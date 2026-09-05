7ac23ffce8e2e31e8c5e63638b236f7a4b70d261
bpgpitt10 <bpgpitt10@gmail.com>
2026-09-05 17:11:13 -0400

    Register all legacy performance shadow models

--- src/intelligence/models/index.ts
+++ src/intelligence/models/index.ts
@@ -1,3 +1,15 @@
+import { clubConfidenceDefinition } from './clubConfidence/definition'
+import { dataConfidenceDefinition } from './dataConfidence/definition'
+import { directionWindowDefinition } from './directionWindow/definition'
+import { distanceWindowDefinition } from './distanceWindow/definition'
+import { flightQualityDefinition } from './flightQuality/definition'
 import { patternStabilityDefinition } from './patternStability/definition'
 
-export const intelligenceModelDefinitions = [patternStabilityDefinition] as const
+export const intelligenceModelDefinitions = [
+  distanceWindowDefinition,
+  directionWindowDefinition,
+  flightQualityDefinition,
+  patternStabilityDefinition,
+  dataConfidenceDefinition,
+  clubConfidenceDefinition,
+] as const
