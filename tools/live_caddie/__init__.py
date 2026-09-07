0183329037d3a2ffa8740f335d292556651e9723
bpgpitt10 <bpgpitt10@gmail.com>
2026-09-07 13:29:18 -0400

    Expose orchestration and source-resolution contracts

--- tools/live_caddie/__init__.py
+++ tools/live_caddie/__init__.py
@@ -1,8 +1,11 @@
 from .assumptions import Assumptions
 from .engine import recommend
 from .round_tracker import RoundTracker
+from .round_orchestrator import PlannedAction, RoundObservation, RoundOrchestrator
 from .tee_state import TeeStateDecision, TeeStateInputs, infer_tee_state
 from .shot_progression import ShotProgressionDecision, ShotProgressionInputs, infer_shot_progression
+from .source_resolution import ResolvedDistance, resolve_distance_to_pin, resolve_identity
+from .aim_geometry import AimContext2D, AimOffsetVerification, cross_track_delta_yds, verify_requested_offset
 from .models import (
     PointYards,
     HazardBoundary,
@@ -18,12 +21,22 @@ __all__ = [
     "Assumptions",
     "recommend",
     "RoundTracker",
+    "RoundObservation",
+    "PlannedAction",
+    "RoundOrchestrator",
     "TeeStateDecision",
     "TeeStateInputs",
     "infer_tee_state",
     "ShotProgressionDecision",
     "ShotProgressionInputs",
     "infer_shot_progression",
+    "ResolvedDistance",
+    "resolve_distance_to_pin",
+    "resolve_identity",
+    "AimContext2D",
+    "AimOffsetVerification",
+    "cross_track_delta_yds",
+    "verify_requested_offset",
     "PointYards",
     "HazardBoundary",
     "GreenSurface",
