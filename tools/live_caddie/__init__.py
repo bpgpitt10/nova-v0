327efb7d5a50e8c7aa5e07ae68d88993116fe75d
bpgpitt10 <bpgpitt10@gmail.com>
2026-09-07 13:47:37 -0400

    Export shot-mode inference contract

--- tools/live_caddie/__init__.py
+++ tools/live_caddie/__init__.py
@@ -4,6 +4,7 @@ from .round_tracker import RoundTracker
 from .round_orchestrator import PlannedAction, RoundObservation, RoundOrchestrator
 from .tee_state import TeeStateDecision, TeeStateInputs, infer_tee_state
 from .shot_progression import ShotProgressionDecision, ShotProgressionInputs, infer_shot_progression
+from .shot_mode import ShotModeDecision, ShotModeInputs, infer_shot_mode, polygon_from_canonical_hole
 from .source_resolution import ResolvedDistance, resolve_distance_to_pin, resolve_identity
 from .aim_geometry import AimContext2D, AimOffsetVerification, cross_track_delta_yds, verify_requested_offset
 from .models import (
@@ -30,6 +31,10 @@ __all__ = [
     "ShotProgressionDecision",
     "ShotProgressionInputs",
     "infer_shot_progression",
+    "ShotModeDecision",
+    "ShotModeInputs",
+    "infer_shot_mode",
+    "polygon_from_canonical_hole",
     "ResolvedDistance",
     "resolve_distance_to_pin",
     "resolve_identity",
