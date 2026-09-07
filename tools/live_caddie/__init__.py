bd7d3623b2e5cba7f0c355a83c63067a9946e054
bpgpitt10 <bpgpitt10@gmail.com>
2026-09-07 12:41:02 -0400

    Expose shot progression contract

--- tools/live_caddie/__init__.py
+++ tools/live_caddie/__init__.py
@@ -2,6 +2,7 @@ from .assumptions import Assumptions
 from .engine import recommend
 from .round_tracker import RoundTracker
 from .tee_state import TeeStateDecision, TeeStateInputs, infer_tee_state
+from .shot_progression import ShotProgressionDecision, ShotProgressionInputs, infer_shot_progression
 from .models import (
     PointYards,
     HazardBoundary,
@@ -20,6 +21,9 @@ __all__ = [
     "TeeStateDecision",
     "TeeStateInputs",
     "infer_tee_state",
+    "ShotProgressionDecision",
+    "ShotProgressionInputs",
+    "infer_shot_progression",
     "PointYards",
     "HazardBoundary",
     "GreenSurface",
