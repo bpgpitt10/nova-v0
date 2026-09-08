445df39218b083437d799c1d5fc061915653edf3
bpgpitt10 <bpgpitt10@gmail.com>
2026-09-08 11:13:45 -0400

    Hide blind mishit review behind tester query flag

--- src/pages/SessionIntelligencePage.tsx
+++ src/pages/SessionIntelligencePage.tsx
@@ -1,10 +1,16 @@
 import App from '../App'
+import ShotReviewPanel from '../components/ShotReviewPanel'
 import './SessionIntelligencePage.css'
 
 function SessionIntelligencePage() {
+  const showMishitReview =
+    typeof window !== 'undefined' &&
+    new URLSearchParams(window.location.search).get('mishitReview') === '1'
+
   return (
     <div className="session-intelligence-page">
       <App forceSessionIntelligenceRoute />
+      {showMishitReview ? <ShotReviewPanel /> : null}
     </div>
   )
 }
